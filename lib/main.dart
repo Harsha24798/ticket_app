dfec3f822b546bbd90cd356f2978229c2bc5a66f
Harsha24798 <harshamadushan24798@gmail.com>
2025-12-29 15:26:10 +0530

    Refactor MyHomePage to streamline widget structure and enhance UI consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,56 +18,63 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            // Handle menu button press
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
             onPressed: () {
-              // Handle search button press
+              // Handle menu button press
             },
           ),
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {
-              // Handle notifications button press
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {
-              // Handle notifications button press
-            },
-          ),
-        ],
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 33, 243, 135),
-                const Color.fromARGB(255, 126, 176, 39),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Handle search button press
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                // Handle notifications button press
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {
+                // Handle notifications button press
+              },
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  const Color.fromARGB(255, 33, 243, 135),
+                  const Color.fromARGB(255, 126, 176, 39),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
+          title: Text('Pos System'),
+          backgroundColor: Colors.blueAccent,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.settings)),
+              Tab(icon: Icon(Icons.person)),
+            ],
+          ),
         ),
-        title: Text('Pos System'),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        body: TabBarView(
           children: [
-            Text('Welcome to the POS System!', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 20),
-            Image(image: AssetImage('assets/login.jpg'), fit: BoxFit.cover),
+            Icon(Icons.home),
+            Icon(Icons.settings),
+            Icon(Icons.person),
           ],
         ),
       ),
